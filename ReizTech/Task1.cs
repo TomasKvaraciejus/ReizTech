671c055f395c13800552230c0a4250892ddbe017
TomasKvaraciejus <tom.kvaraciejus@gmail.com>
2023-02-04 10:58:57 +0200

    Created repository

---
+++ ReizTech/Task1.cs
@@ -0,0 +1,36 @@
+﻿// 1 - Calculating lesser angle between hour and minute hands on analogue clock
+
+try
+{
+    int minutes;
+    float hours, angle;
+    string input;
+
+    Console.WriteLine("Enter hour value:"); // Input must be integer between 0-24 (values above 12 will be lowered to 0-12 range)
+    input = Console.ReadLine();
+    hours = Int32.Parse(input);
+    if (hours > 12)
+        hours -= 12;
+    if (hours > 12 || hours < 0)
+        throw new Exception("Hour hand out of range");
+
+    Console.WriteLine("Enter minute value:"); // Input must be integer between 0-60
+    input = Console.ReadLine();
+    minutes = Int32.Parse(input);
+    if (minutes > 60 || minutes < 0)
+        throw new Exception("Minute hand out of range");
+
+    Console.WriteLine($"Time on clock is {hours}h {minutes}min");
+
+    hours += (float)minutes / 60;
+
+    angle = Math.Abs((hours * 5) - minutes) * 6;
+    if (angle > 180)
+        angle = 360 - angle;
+
+    Console.WriteLine($"\nLesser angle between hour and minute hands is {Math.Round(angle, 2)}deg");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"err: {ex.Message}");
+}
---
+++ ReizTech/Task2-3.cs
@@ -0,0 +1,64 @@
+﻿// 2-3 - Calculating maximum depth of Branch
+
+namespace ReizTech
+{
+    class Task2_3
+    {
+        public static string input;
+
+        class Branch
+        {
+            public List<Branch> branches = new List<Branch>(); // I assume that setting branches to public is allowed
+        }
+
+        public static void Main(string[] args)
+        {
+            Branch head = new Branch();
+
+            /*head.branches.AddRange(new List<Branch>{ new Branch(), new Branch() }); // Discrete way to populate tree
+
+            head.branches[0].branches.Add(new Branch());
+            head.branches[1].branches.AddRange(new List<Branch> { new Branch(), new Branch(), new Branch() });
+
+            head.branches[1].branches[0].branches.Add(new Branch());
+            head.branches[1].branches[1].branches.AddRange(new List<Branch> { new Branch(), new Branch() });
+            head.branches[1].branches[2].branches.Add(new Branch());
+
+            head.branches[1].branches[1].branches[0].branches.Add(new Branch());*/
+
+            input = "2 1 0 3 1 0 2 1 0 1 0 1 0"; // input string corresponds to tree presented in Task 2
+            head = populateTree();
+
+            Console.WriteLine($"Depth of Branch is {getDepth(head)}");
+        }
+
+        static Branch populateTree() // Populates tree according to string input - first character in input is branch count (limited to 9 per branch), tree is populated left to right
+        {
+            Branch head = new Branch();
+            int newBranches = input[0] - 48;
+
+            for (int i = 0; i < newBranches; ++i)
+            {
+                input = input.Substring(2);
+                Branch nBranch = populateTree();
+                head.branches.Add(nBranch);
+            }
+
+            return head;
+        }
+
+        static int getDepth(Branch head) // Recursively calculates maximum depth of Branch
+        {
+            int maxDepth = 0;
+
+            foreach(Branch b in head.branches)
+            {
+                int curDepth = getDepth(b);
+                if (curDepth > maxDepth)
+                    maxDepth = curDepth;
+            }
+
+            return maxDepth + 1;
+        }
+    }
+}
